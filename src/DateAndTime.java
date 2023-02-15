ee2506c126370818fcc19eed7f33df957453cfa4
Ramiro2529 <ramitas1597@gmail.com>
2023-02-14 18:22:23 -0600

    Date time challenge

---
+++ src/DateAndTime.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.Calendar;
+
+public class DateAndTime {
+
+
+    public static String findDay(int month, int day, int year) {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month-1, day);
+        int dayOutPut = calendar.get(Calendar.DATE);
+        int monthOutPut = calendar.get(Calendar.MONTH);
+        int yearOutPut = calendar.get(Calendar.YEAR);
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+
+        System.out.println(day + " " + month + " " + year);
+        System.out.println("dayOfWeek = " + dayOfWeek);
+
+        switch (dayOfWeek) {
+            case 1:
+                return "SUNDAY";
+            case 2:
+                return "MONDAY";
+            case 3:
+                return "TUESDAY";
+            case 4:
+                return "WEDNESDAY";
+            case 5:
+                return "THURSDAY";
+            case 6:
+                return "FRIDAY";
+            case 7:
+                return "SATURDAY";
+            default:
+        }
+
+        return null;
+    }
+
+
+}
+
--- src/IntToString.java
+++ src/IntToString.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class IntToString {
 
     public static void main(String[] args) {
+
+        try {
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            in.close();
+            String s = String.valueOf(n);
+            boolean result=s instanceof String;
+
+            if (result) {
+                System.out.println("Good job");
+            } else {
+                throw new Exception("Wrong answer");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        /*if (n == Integer.parseInt(s)) {
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer.");
+        }
+    } catch (DoNotTerminate.ExitTrappedException e) {
+        System.out.println("Unsuccessful Termination!!");
+    }*/
 
 
     }
